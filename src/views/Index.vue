3eaa82df45149553930711b5b3a12b50f1eb4221
mac <mac@Ones-Macbook-Pro.local>
2021-12-22 11:12:59 +0100

    updates made

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,6 +2,12 @@
   <div>
     <p>Home page</p>
     <router-link :to="{ name: 'login' }">Go to Login page</router-link>
+    &nbsp;
+    <router-link :to="{ name: 'resetPassword' }"
+      >Go to reset password</router-link
+    >
+    &nbsp;
+    <router-link :to="{ name: 'verifyEmail' }">Go to verify email</router-link>
   </div>
 </template>
 
